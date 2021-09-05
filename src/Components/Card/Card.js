93d16b241af2c73cf4e172733ac7554b17fa2430
ItsZumar <zumarsaeed17@gmail.com>
2021-09-05 18:32:30 +0500

    changes in cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,21 +1,39 @@
 import React from "react";
 import "./Card.css";
-import cardData from "../../DummyData/card";
+import location from "../../Icons/placeholder.svg";
+import email from "../../Icons/mail.svg";
+import phone from "../../Icons/telephone.svg";
 
 function Card() {
   return (
     <div className="cardCon">
-      {cardData.map((el) => (
-        <div className="card">
-          <div className="iconCon">
-            <img src={el.icon} alt="#" className="icons" />
-          </div>
-          <div className="detailCon">
-            <span className="cardHeading">{el.heading}</span>
-            <span className="cardDetail">{el.detail}</span>
-          </div>
+      <div className="card">
+        <div className="iconCon">
+          <img src={location} alt="#" className="icons" />
         </div>
-      ))}
+        <div className="detailCon">
+          <span className="cardHeading">Adress</span>
+          <span className="cardDetail">Rawalpindi, PK</span>
+        </div>
+      </div>
+      <div className="card">
+        <div className="iconCon">
+          <img src={email} alt="#" className="icons" />
+        </div>
+        <div className="detailCon">
+          <span className="cardHeading">Email</span>
+          <span className="cardDetail">zumarsaeed17@gmail.com</span>
+        </div>
+      </div>
+      <div className="card">
+        <div className="iconCon">
+          <img src={phone} alt="#" className="icons" />
+        </div>
+        <div className="detailCon">
+          <span className="cardHeading">Phone Number</span>
+          <span className="cardDetail">0888-676677</span>
+        </div>
+      </div>
     </div>
   );
 }
