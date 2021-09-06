0da7a6ddc4e0cea55a197b7ed6322419c0235063
ItsZumar <zumarsaeed17@gmail.com>
2021-09-06 19:25:15 +0500

    education done

--- src/sections/Education/Education.js
+++ src/sections/Education/Education.js
@@ -10,28 +10,30 @@ function Education() {
       <div className="educationStyle"></div>
       <div className="educationContent">
         <div className="educationDetail">
-          <p className="educationInfo">
-            <span className="year">2015-2017</span>
-            <span className="InstitueName"> Oxford Model School</span>
-            <span>I passed my 9th and matriculation in 2017.</span>
-          </p>
-          <p className="educationInfo">
-            <span className="year">2017-2019</span>
-            <span className="InstitueName">
-              Post Graduate College Rawlpindi
-            </span>
-            <span>
-              I did FSc From Post Graduate College from Rawlpindi in 2019.
-            </span>
-          </p>
-          <p className="educationInfo">
-            <span className="year">2019-TillNow</span>
-            <span className="InstitueName"> Rawlpindi Women University</span>
-            <span>
-              Now I am doing Bachelors in Computer Sciences from Rawalpindi
-              Women University.
-            </span>
-          </p>
+          <div className="styling">
+            <p className="educationInfoSchool">
+              <span className="year">2015-2017</span>
+              <span className="InstitueName"> Oxford Model School</span>
+              <span>I passed my 9th and matriculation in 2017.</span>
+            </p>
+            <p className="educationInfoCllg">
+              <span className="year">2017-2019</span>
+              <span className="InstitueName">
+                Post Graduate College Rawlpindi
+              </span>
+              <span>
+                I did FSc From Post Graduate College from Rawlpindi in 2019.
+              </span>
+            </p>
+            <p className="educationInfoUni">
+              <span className="year">2019-TillNow</span>
+              <span className="InstitueName"> Rawlpindi Women University</span>
+              <span>
+                Now I am doing Bachelors in Computer Sciences from Rawalpindi
+                Women University.
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
