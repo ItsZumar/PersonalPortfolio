d53ce7a408047dc7df0921366e748a3c95a590ba
ItsZumar <zumarsaeed17@gmail.com>
2021-09-12 18:47:53 +0500

    last commit

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,37 +3,55 @@ import "./Card.css";
 import { MdLocationOn } from "react-icons/md";
 import { AiFillMail } from "react-icons/ai";
 import { FaPhoneAlt } from "react-icons/fa";
+import { useSpring, animated } from "react-spring";
 
 function Card() {
+  const props = useSpring({
+    to: { opacity: 1, marginLeft: 0 },
+    from: { opacity: 0, marginLeft: 10000 },
+    delay: 350,
+  });
   return (
     <div className="cardCon">
-      <div className="card">
-        <div className="iconCon1">
-          <MdLocationOn className="icons" />
+      <animated.div style={props}>
+        <div style={props}>
+          <div className="card">
+            <div className="iconCon1">
+              <MdLocationOn className="icons" />
+            </div>
+            <div className="detailCon">
+              <span className="cardHeading">Adress</span>
+              <span className="cardDetail">Rawalpindi, PK</span>
+            </div>
+          </div>
         </div>
-        <div className="detailCon">
-          <span className="cardHeading">Adress</span>
-          <span className="cardDetail">Rawalpindi, PK</span>
+      </animated.div>
+      <animated.div style={props}>
+        <div style={props}>
+          <div className="card">
+            <div className="iconCon2">
+              <AiFillMail className="icons" />
+            </div>
+            <div className="detailCon">
+              <span className="cardHeading">Email</span>
+              <span className="cardDetail">zumarsaeed17@gmail.com</span>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="card">
-        <div className="iconCon2">
-          <AiFillMail className="icons" />
+      </animated.div>
+      <animated.div style={props}>
+        <div style={props}>
+          <div className="card">
+            <div className="iconCon3">
+              <FaPhoneAlt className="icons" />
+            </div>
+            <div className="detailCon">
+              <span className="cardHeading">Phone Number</span>
+              <span className="cardDetail">0888-676677</span>
+            </div>
+          </div>
         </div>
-        <div className="detailCon">
-          <span className="cardHeading">Email</span>
-          <span className="cardDetail">zumarsaeed17@gmail.com</span>
-        </div>
-      </div>
-      <div className="card">
-        <div className="iconCon3">
-          <FaPhoneAlt className="icons" />
-        </div>
-        <div className="detailCon">
-          <span className="cardHeading">Phone Number</span>
-          <span className="cardDetail">0888-676677</span>
-        </div>
-      </div>
+      </animated.div>
     </div>
   );
 }
--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -2,17 +2,26 @@ import React from "react";
 import "./About.css";
 import illutration from "../../Image/girl-ill.png";
 import Card from "../../Components/Card/Card";
+import { useSpring, animated } from "react-spring";
 
 const aboutContent = `<Hi there! I’m Zumar. />`;
 
 function About() {
+  const props = useSpring({
+    to: { opacity: 1, marginLeft: 0 },
+    from: { opacity: 0, marginLeft: -500 },
+    delay: 150,
+  });
+
   return (
     <>
       <div className="about" id="aboutSec1">
         <div className="aboutContent">
-          <h1 data-text={aboutContent} className="heading">
-            {aboutContent}
-          </h1>
+          <animated.div style={props}>
+            <div style={props}>
+              <h1 className="heading">{aboutContent}</h1>
+            </div>
+          </animated.div>
           <p className="paragraph">
             I’m a software engineer from Rawalpindi, PK. I’m also a FrontEnd
             Developer & I make elegantly professional Web designs.
@@ -28,6 +37,7 @@ function About() {
           <img src={illutration} alt="#" className="illustration" />
         </div>
       </div>
+
       <div className="container">
         <Card />
       </div>
